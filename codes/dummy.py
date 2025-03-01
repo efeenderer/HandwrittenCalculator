5c2db5ddc270eb80d48d33085fc3c0581f6043bf
Ender Efe <efe.ender.er@gmail.com>
2025-03-01 22:49:38 +0300

    There is an error to be solved. Check the square roots. When two contours are next to each other, in order to detect one of them, the other one gets in the picture too

--- codes/dummy.py
+++ codes/dummy.py
@@ -1,23 +1,42 @@
 import cv2
 import numpy as np
+import os
 
-image = cv2.imread(r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests\uc.png")
-save_path = r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests\Tests"
+images_path = r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests"
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+for images in os.listdir(images_path):
+    
+    if images.split(".")[-1] in ["png","jpg","jpeg","webp"]:
+        
+        image_name = images.split(".")[-2]
 
-blur = cv2.GaussianBlur(gray, (5,5), 0)
+        print(image_name)
 
-_, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        image = cv2.imread(r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests"+f"\\{images}")
 
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        save_path = r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests\Tests"+f"\\{image_name}"
 
-char_images = []
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    char = thresh[y:y+h, x:x+w]
-    char_images.append(char)
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
 
 
-for i, char in enumerate(char_images):
-    cv2.imwrite(f"{save_path}\char_{i}.png", char)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f"{save_path}\\gray.png", gray)
+
+        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        cv2.imwrite(f"{save_path}\\blur.png", blur)
+
+        _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        cv2.imwrite(f"{save_path}\\thresh.png", thresh)
+
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        char_images = []
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            char = thresh[y:y+h, x:x+w]
+            char_images.append(char)
+
+
+        for i, char in enumerate(char_images):
+            cv2.imwrite(f"{save_path}\{i}.png", char)
