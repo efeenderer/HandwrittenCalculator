b8522c3c043201918a46c9c9acd288076c8aedce
Ender Efe <efe.ender.er@gmail.com>
2025-03-06 09:43:09 +0300

    First commit of DLModel branch

--- codes/dummy.py
+++ codes/dummy.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 images_path = r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests"
 
@@ -16,14 +17,12 @@ for images in os.listdir(images_path):
 
         save_path = r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests\Tests"+f"\\{image_name}"
 
-        if not os.path.exists(save_path):
-            os.mkdir(save_path)
-
+        os.makedirs(save_path, exist_ok=True)
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         cv2.imwrite(f"{save_path}\\gray.png", gray)
 
-        blur = cv2.GaussianBlur(gray, (5,5), 0)
+        blur = cv2.GaussianBlur(gray, (3,3), 0)
         cv2.imwrite(f"{save_path}\\blur.png", blur)
 
         _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
@@ -31,12 +30,33 @@ for images in os.listdir(images_path):
 
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        #print(f"Contours is this: {contours}")
+        #print(f"A single contour: {contours[0]}")
+        #print(f"Its type is: {type(contours[0])}")
+        
         char_images = []
+        only_contours = []
         for contour in contours:
+
+            min_x = min(point[0][0] for point in contour)
+            max_x = max(point[0][0] for point in contour)
+            min_y = min(point[0][1] for point in contour)
+            max_y = max(point[0][1] for point in contour)
+
+            contour_image_width = max_x - min_x + 1
+            contour_image_height = max_y - min_y + 1
+            contour_image = np.zeros((contour_image_height, contour_image_width), dtype=np.uint8)
+
+            for point in contour:
+                x, y = point[0]
+                contour_image[y - min_y, x - min_x] = 255
+
             x, y, w, h = cv2.boundingRect(contour)
             char = thresh[y:y+h, x:x+w]
-            char_images.append(char)
 
+            char_images.append(char)
+            only_contours.append(contour_image)
 
         for i, char in enumerate(char_images):
-            cv2.imwrite(f"{save_path}\{i}.png", char)
+            cv2.imwrite(f"{save_path}\{i}_cut.png", char)
+            cv2.imwrite(f"{save_path}\{i}_contour.png", only_contours[i])
