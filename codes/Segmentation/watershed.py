4498e33d4be92d2bd7da4e84dd87b4192c166476
Ender Efe <efe.ender.er@gmail.com>
2025-03-14 12:41:49 +0300

    14.03

--- codes/Segmentation/watershed.py
+++ codes/Segmentation/watershed.py
@@ -8,5 +8,5 @@ for images in os.listdir(images_path):
     
     if images.split(".")[-1] in ["png","jpg","jpeg","webp"]:
         
-        image = cv2.imread(r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests"+f"\\{images}", cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(r"E:\Python_Projeler\ComputerVisionProjects\FinalProject\codes\ParsingTests"+f"\\{images}")
 
