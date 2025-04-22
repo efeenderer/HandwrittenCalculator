abdea22f9e772ebc3570840ed4a9b825c25c1fc1
Ender Efe <efe.ender.er@gmail.com>
2025-04-22 20:25:59 +0300

    line enlarging problem is solved. Dataset creation is starting

--- codes/ModelCodes/datasetCreationCodes/dataset_creation.py
+++ codes/ModelCodes/datasetCreationCodes/dataset_creation.py
@@ -14,16 +14,35 @@ for index, letter_page in enumerate(os.listdir(letter_pages_path)):
     page = cv2.imread(letter_path, cv2.IMREAD_GRAYSCALE)
 
     blur = cv2.GaussianBlur(page,(3,3),1)
+
     _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = np.ones((2,2), np.uint8)
+
+    dilation = cv2.dilate(thresh, kernel, iterations=1)
+
+
+    contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     
-    if index == 0:
-        continue
+
+    
+
+    if index < 38:   continue
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        letter = thresh[y:y+h, x:x+w]
-        letter = cv2.resize(letter, (64,64))
+
+        max_edge = max(h,w)
+        
+        blank = np.zeros((max_edge,max_edge),np.uint8)
+
+        x1,x2 = int( (max_edge - h) / 2 ), int( (max_edge + h) / 2 )
+        y1,y2 = int( (max_edge - w) / 2 ), int( (max_edge + w) / 2 )
+
+        blank[x1:x2,y1:y2] = dilation[y:y+h, x:x+w]
+        letter = cv2.resize(blank, (64,64))
+        
+
         cv2.namedWindow("lala", cv2.WINDOW_FREERATIO)
         cv2.imshow("lala", letter)
         cv2.waitKey(0)
